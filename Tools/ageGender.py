bbce6ac6c8a159fa4e4d25352ed126039f083343
Pavel Kazantsev <pakazancev901@gmail.com>
2024-03-17 20:23:18 +0400

    Xception Metrics add

---
+++ Tools/ageGender.py
@@ -0,0 +1,90 @@
+import sys
+from age_and_gender import AgeAndGender
+from PIL import Image
+import os
+
+def is_valid_image(file_path):
+    try:
+        img = Image.open(file_path)
+        img.verify()  # Проверяем целостность файла
+        return True
+    except (IOError, SyntaxError):
+        return False
+
+
+def resize_and_crop_image(img, target_size, face_coordinates):
+    # Открываем изображение
+    #img = Image.open(image_path)
+    
+    # Рассчитываем соотношение сторон исходного изображения
+    width, height = img.size
+    aspect_ratio = width / height
+    # Определяем меньшую сторону и масштабируем её до 2048, а другую пропорционально
+    if aspect_ratio > 1:  # ширина больше высоты
+        new_height = target_size
+        new_width = int(new_height * aspect_ratio)
+    else:  # высота больше ширины
+        new_width = target_size
+        new_height = int(new_width / aspect_ratio)
+    # Масштабируем изображение
+    img = img.resize((new_width, new_height))
+
+    left_top_x, left_top_y, right_bottom_x, right_bottom_y = face_coordinates
+    difference_first = left_top_y
+    difference_second = height - right_bottom_y
+
+    #Обрезаем изображение до квадрата
+    left = (new_width - target_size) / 2 #Не трогать
+    top = (new_height - target_size) / 2
+    right = (new_width + target_size) / 2 #Не трогать
+    bottom = (new_height + target_size) / 2
+
+    if int(left) == 0 and int(right) == target_size and difference_second > difference_first:
+      bottom = bottom - top
+      top = top - top
+      
+    img = img.crop((left, top, right, bottom))
+    return img
+
+
+data = AgeAndGender()
+
+modelsPath = "UbuntuParts/AgeGenderDetector/example/models/"
+
+data.load_shape_predictor(f'{modelsPath}shape_predictor_5_face_landmarks.dat')
+data.load_dnn_gender_classifier(f'{modelsPath}dnn_gender_classifier_v1.dat')
+data.load_dnn_age_predictor(f'{modelsPath}dnn_age_predictor_v1.dat')
+
+dataSetsPath = "/media/pavel/5B99-804C/Datasets/aboba.png"
+DATA_SAVE_PATH = "sfdfssdfs/"
+target_size = 1024
+directory_path = "dsdf"
+
+for filename in os.listdir(directory_path):
+    file_path = os.path.join(directory_path, filename)
+    if not is_valid_image(file_path):
+        os.remove(file_path)
+        print(f"Deleted invalid JPG file: {file_path}")
+    
+    try:
+        image = Image.open(file_path)
+        result = data.predict(image)
+        object_count = len(result)
+        # Проверяем, если объект только один, получаем age и gender
+        if object_count == 1:
+            age = result[0]['age']['value']
+            gender = result[0]['gender']['value']
+            face = result[0]['face']['value']
+            print(f"Возраст: {age}, Пол: {gender}")
+            if age < 18:
+                os.remove(file_path)
+                print(f"Deleted img with age < 18: {file_path}")
+                continue
+            save_image = resize_and_crop_image(image, target_size, face)
+            save_image.save(f'{DATA_SAVE_PATH}/f{filename}')
+        else:
+            os.remove(file_path)
+            print(f"Deleted img with any Faces: {file_path}")
+    except Exception as e:
+        print(f"Error processing image {file_path}: {e}")
+        os.remove(file_path)
--- Tools/reshape.py
+++ Tools/reshape.py
@@ -1,8 +1,9 @@
-import imutils
+#import imutils
+from warnings import catch_warnings
 import cv2
 import numpy as np
 from PIL import Image
-
+import os 
 
 #reshape с сохранением пропорций картинки
 def reshape_save_proporshions(file_path, file_path_save, width = 256, height = 256):
@@ -49,36 +50,47 @@ def resize_hard_image(file_path, file_path_save, size=(256,256)):
 
 
 # Разделение изображения на n частей
-def cut_n_image_from_image(file_path, filepath_save, name):
+def cut_n_image_from_image(file_path, filepath_save, file_name):
     img = cv2.imread(file_path)
     dimensions = img.shape
     # харкодом под 1024, для e4e можно будет потом в параметр
     N = dimensions[1] // 1024
     width_cutoff = dimensions[1] // N
     
-    list_img = list()
+    #list_img = list()
     for i in range(N):
-        counter = i * width_cutoff
-        img_save = img[:, counter:counter + width_cutoff]
-        list_img.append(img_save)
-        cv2.imwrite(f"{filepath_save}\\{name}{i}.png", img_save)
+        try:
+            if not os.path.exists(f"{filepath_save}\\{i}"): 
+                os.makedirs(f"{filepath_save}\\{str(i)}")         
+            counter = i * width_cutoff
+            img_save = img[:, counter:counter + width_cutoff]
+            #list_img.append(img_save)
+            cv2.imwrite(f"{filepath_save}\\{str(i)}\\{file_name}{str(i)}.png", img_save)
+        except:
+            print(f"Error processing image {file_path}: ")
+    print("saved images")
 
 
+def cut_n_image_root(root_directory, filepath_save):
+    for filename in os.listdir(root_directory):
+        file_path = os.path.join(root_directory, filename)
+        cut_n_image_from_image(file_path, filepath_save, filename)
 
-file_path = "ReshapeImage\\input_image\\tones.png"
-file_path_save = "ReshapeImage\\output_image"
+
+file_path_dir = "E:\\Encoder4EditingNOCUT\\E4E\\fake"
+file_path_save = "E:\\e4e\\fake"
 
 # Пример использования
-input_image_path = 'F:\\DataBaseImage\\ffhq\\00868.png'
-output_image_path = '00868_process_3IMU.png'
-target_size = (2048, 2048)
+#input_image_path = 'F:\\DataBaseImage\\ffhq\\00868.png'
+#output_image_path = '00868_process_3IMU.png'
+#target_size = (2048, 2048)
 
 #resize_image_save_proporshions(input_image_path, output_image_path, target_size)
 #resize_hard_image(input_image_path, output_image_path, target_size)
-reshape_save_proporshions(input_image_path, output_image_path, 2048, 2048)
+#reshape_save_proporshions(input_image_path, output_image_path, 2048, 2048)
 
 
-#cut_n_image_from_image(file_path, file_path_save, "tone")
+cut_n_image_root(file_path_dir, file_path_save)
 
 
 #cv2.imshow('image' , img)
